8d43d314041682e22c729d13dd58458c911aabee
Steve Anderson <prototype909@msn.com>
2020-05-17 22:14:54 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,10 +5,10 @@
 
 class EmailAddressParser
 
-  attr_accessor :emails
+  attr_accessor :email
 
-  def initialize(emails)
-    @emails = email
+  def initialize(email)
+    @email = email
   end
 
   def parse
