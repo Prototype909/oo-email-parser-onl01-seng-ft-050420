5bf4c645a576aad7e3dc91b60e0b265b5dcae430
Steve Anderson <prototype909@msn.com>
2020-05-17 22:24:52 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,9 +12,10 @@ class EmailAddressParser
   end
 
   def parse
-    email_array = @email.split(/[, ]/).uniq
-    email_array.reject! {|element| element.empty?}
-    email_array
+    email.split.collect do |address|
+      address.split(',')
+    end
+    .flatten.uniq
   end
 
 end
